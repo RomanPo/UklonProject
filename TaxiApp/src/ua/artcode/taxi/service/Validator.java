e302a76e14bb8b2858ab04945724128d762c6f46
Tetiana Osmanova <t.osmanova@gmail.com>
2016-04-24 12:10:28 +0300

    Add validator for password

---
+++ TaxiApp/src/ua/artcode/taxi/service/Validator.java
@@ -0,0 +1,14 @@
+package ua.artcode.taxi.service;
+
+import ua.artcode.taxi.dao.AppDB;
+import ua.artcode.taxi.model.User;
+
+/**
+ * Created by tos on 4/24/16.
+ */
+public class Validator {
+    public boolean validatePassword(AppDB appDB, String phone, String password){
+        
+        return true;
+    }
+}
